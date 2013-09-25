ecb7403228edf5176dda761e544aa2034454abef
Trey Simmons <yert511@gmail.com>
2013-09-24 20:19:03 -0500

    Added "Model.php" file in core directory

---
+++ system/core/Model.php
@@ -0,0 +1,20 @@
+<?php
+
+	/*
+	
+		BaseModel
+		
+		The BaseModel class will be the superclass of every application model
+			used with this framework. It includes default functionality for 
+			every model used in a framework-leveraging application.
+	
+	*/
+	
+	class BaseModel {
+	
+	
+	
+	}// end BaseModel Class
+	
+/* End of File "Model.php" */
+/* Location: "./system/core/Model.php" */
