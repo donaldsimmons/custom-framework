432b9fb28e5faeb2b67282824eec5dde88c80d79
Trey Simmons <yert511@gmail.com>
2013-09-24 20:19:41 -0500

    Added "Router.php" file in core directory

---
+++ system/core/Router.php
@@ -0,0 +1,19 @@
+<?php
+
+	/*
+	
+		Router 
+		
+		The Router class will interpret URLs and route application functionality
+			according to URL request contents.
+	
+	*/
+	
+	class Router {
+	
+	
+	
+	}// end Router Class
+
+/* End of File "Router.php" */
+/* Location: "./system/core/Router.php" */
