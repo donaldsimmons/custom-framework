a9296277633aa41ebc0305cd2953b4bfae8fdd4a
Trey Simmons <yert511@gmail.com>
2013-09-24 20:17:43 -0500

    Added "Controller.php" file in core directory

---
+++ system/core/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+	/*
+	
+		BaseController
+		
+		The BaseController class will be a superclass of every application 
+			controller used with the framework. It contains default functionality
+			necessary for every controller to perform its essential operations
+	
+	*/
+	
+	class BaseController {
+	
+	
+	
+	}// end BaseController Class
+	
+/* End of File "Controller.php"  */
+/* Location: "./system/core/Controller.php" */
