165f0f43fc1a4be0e39a3132c22f3982a83fe3de
Trey Simmons <yert511@gmail.com>
2013-09-24 20:18:32 -0500

    Added "Loader.php" file in core directory

---
+++ system/core/Loader.php
@@ -0,0 +1,18 @@
+<?php
+
+	/*
+	
+		Loader
+		
+		The Loader class will load nessary classes for inclusion in controllers.
+	
+	*/
+	
+	class Loader {
+	
+	
+	
+	}// end Loader Class
+	
+/* End of File "Loader.php" */
+/* Location: "./system/core/Loader.php" */
