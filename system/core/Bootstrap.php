bcd0ed352f4f45945ec36485cd429fa2a1fc4c22
Trey Simmons <yert511@gmail.com>
2013-09-26 15:41:53 -0500

    Added "Bootstrap.php" file to "core" folder - will act as initialization file for framework

---
+++ system/core/Bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+	/*
+	
+		Bootstrap
+		
+		This is the initialization file for the framework.
+		
+		It will load the base classes for for the framework and execute the 
+			requested functionlaity.
+	
+	*/
