83746ab911181301799e74ab6123b963d10adc65
Trey Simmons <yert511@gmail.com>
2013-09-26 23:11:12 -0500

    Added commenting for "loadClass" function

--- system/core/Loader.php
+++ system/core/Loader.php
@@ -18,22 +18,40 @@
 		# requires requested file, and returns an instance of the required class
 		public function &loadClass($class,$directory) {
 			
+			/*
+				
+				uses "&loadClass" vs "loadClass" so that function returns reference to
+					the object returned instead of the value
+					
+				this means the actual loaded class object is returned, not just the
+					value of the object
+			
+			*/
+			
+			# uses static array to maintain array values for already loaded classes,
+			#	even after program execution leaves this function's scope
 			static $loaded_classes = array();
 			
+			
+			# if class is already loaded, don't load again - return loaded object
 			if(isset($loaded_classes[$class])) {
 				
 				return $loaded_classes[$class];
 			
 			}
 			
+			
 			$found_path = FALSE;
 			
 			$possible_paths = [APP_PATH,SYSTEM_PATH];
 			
+			# check each directory option for the request directory and file
 			foreach($possible_paths as $path) {
 				
 				if(file_exists($path.$directory.'/'.$class.'.php')) {
 					
+					
+					# if found, require the file, and indicate that it has been found
 					require $path.$directory.'/'.$class.'.php';
 					
 					$found_path = TRUE;
@@ -44,12 +62,15 @@
 				
 			}
 			
+			
+			# if the requested class couldn't be located, exit script execution/send message
 			if($found_path === FALSE) {
 				
 				exit("Couldn't find the desired file. Check: ".SELF);
 				
 			}
 		
+			# store the loaded class and return the new instance for use
 			$loaded_classes[$class] = new $class();
 			return $loaded_classes[$class];
 		
